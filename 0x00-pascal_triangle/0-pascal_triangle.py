15468fc180debf21c9d8520d72491ab9ec13a51d
Faith-qa <atienofaith12@gmail.com>
2021-08-05 04:25:58 +0300

    pep8 errors corrected

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """returns a pascal triangle"""
 
+
 def pascal_triangle(n):
     """
     returns a list of integers in a pascal triangle format
@@ -19,7 +20,7 @@ def pascal_triangle(n):
         in_pas = []
         for j in range(i + 1):
             num = 0
-            if left > -1 :
+            if left > -1:
                 num += pascal[i - 1][left]
             if right < i:
                 num += pascal[i - 1][right]
